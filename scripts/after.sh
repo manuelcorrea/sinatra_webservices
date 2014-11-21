56f7b8991b62a35124a7987c655f5a157111a0bd
mcorrea <manuel.correa@careerbuilder.com>
2014-11-21 13:49:19 -0500

    sudo 2

--- scripts/after.sh
+++ scripts/after.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 cd /opt/sinatra_app
-sudo source /usr/local/rvm/scripts/rvm
-sudo rvm use 2.1.1
+source /usr/local/rvm/scripts/rvm
+rvm use 2.1.1
+sudo gem install bundle
 sudo bundle install
 
 sudo cp config/sinatra_app.conf /opt/nginx/sites-available/
