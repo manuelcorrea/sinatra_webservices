c81750d856f410357c8702d14e2210bba713bfea
mcorrea <manuel.correa@careerbuilder.com>
2014-11-21 13:38:13 -0500

    after script

--- scripts/after.sh
+++ scripts/after.sh
@@ -1,1 +1,10 @@
-#!/bin/sh
+#!/bin/bash
+
+cd /opt/sinatra_app
+source /usr/local/rvm/scripts/rvm
+rvm use 2.1.1
+bundle install
+
+sudo cp config/sinatra_app.conf /opt/nginx/sites-available/
+sudo ln -s /opt/nginx/sites-available/sinatra_app.conf  /opt/nginx/sites-enabled/sinatra_app.conf
+sudo service nginx restart
