3bd049319d156f47c9855c6333854c38b982525b
mcorrea <manuel.correa@careerbuilder.com>
2014-11-21 14:32:29 -0500

    changes

--- scripts/after.sh
+++ scripts/after.sh
@@ -9,3 +9,4 @@ bundle install
 sudo cp config/sinatra_app.conf /opt/nginx/sites-available/
 sudo ln -s /opt/nginx/sites-available/sinatra_app.conf  /opt/nginx/sites-enabled/sinatra_app.conf
 sudo service nginx restart
+
